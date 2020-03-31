f353022037b4cd1460125df950c2846e247f84f4
Sushant Pandurangi <sushpa@lav.mavt.ethz.ch>
2020-03-31 18:19:29 +0200

    belongs with previous commit "nice stuff"

--- checkcc/checkstd.h
+++ checkcc/checkstd.h
@@ -6,27 +6,72 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <math.h>
+#include <unistd.h> /* sysconf(3) */
 
+#ifdef WINDOWS
+#include <windows.h>
+#endif
+
+#ifdef WINDOWS
+size_t sys_pageSize()
+{
+    SYSTEM_INFO si;
+    GetSystemInfo(&si);
+    return si.dwPageSize;
+}
+#else
+size_t sys_pageSize()
+{
+    return sysconf(_SC_PAGESIZE); /* _SC_PAGE_SIZE is OK too. */
+}
+#endif
 #include "cycle.h"
+
+// FIXME
+#define _Pool_alloc_(x, y) malloc(y)
 
 #define GB *1024 MB
 #define MB *1024 KB
 #define KB *1024UL
 
+#define Array1D(x) Array1D
+
 #include <sys/resource.h>
-static size_t getstacksize()
+static size_t sys_stackSize()
 {
     struct rlimit limit;
     getrlimit(RLIMIT_STACK, &limit);
     return limit.rlim_cur; //, limit.rlim_max);
 }
 
-#define BTLIMIT 20
+#ifndef NONULLCHECK
+// this is only for idents, not for funcs etc.: those will get evaluated
+// twice if you use DEREF. Have to find another way for them (e.g.
+// a.pop().xyz ) luckily if we stay away from methods and chaining we should
+// be fine. P is for pointer to something, S is string, N is number (default
+// values)
+#define DEREF1(x, y) (x ? x->y : NULL)
+#define DEREF2(x, y, z) (x && x->y ? x->y->z : NULL)
+#define DEREF3(x, y, z, a) (x && x->y && x->y->z ? x->y->z->a : NULL)
+#define DEREF4(x, y, z, a, b)                                              \
+    (x && x->y && x->y->z && x->y->z->a ? x->y->z->a->b : NULL)
+
+#else
+// fast path, all guns blazing, no protection
+#define DEREF1(x, y) x->y
+#define DEREF2(x, y, z) x->y->z
+#define DEREF3(x, y, z, a) x->y->z->a
+#define DEREF4(x, y, z, a, b) x->y->z->a->b
+
+#endif
+
+#define BTLIMIT 5
 #define ERROR_TRACE (char*)0xFFFFFFFFFFFFFFFF
 #define DOBACKTRACE                                                        \
     {                                                                      \
-        *err_ = ERROR_TRACE;                                               \
-        goto backtrace;                                                    \
+        err_ = ERROR_TRACE;                                                \
+        goto done;                                                         \
     }
 // ulimit -s is 8192KB on Life, STACKMAX 8184 is last until which
 // stackoverflow check works after that segfault
@@ -36,45 +81,54 @@ static size_t STACKMAX;
 typedef int Int;
 typedef int Scalar;
 typedef char** Strs;
+typedef char* String;
 #define DF_
+
+// output funcs: print -> normal print, debug -> only prints (to stderr) in
+// debug mode, error -> print to stderr, fatal -> print to stderr and exit
 #define print printf
 
-static void start(const Strs args,
+static void start(const Strs args
 #ifdef DEBUG
-    const char* file_, const int line_,
+    ,
+    const char* callsite_
 #endif
-    const char** err_);
+);
 
 static const char* stackstart;
 static int stackdepth = 0;
+static int stackOverflowDepthThreshold = 0;
+
+static const char* err_ = NULL;
 
 int main(int argc, char* argv[])
 {
-    const char* nul = NULL;
-    const char** err_ = &nul;
+    // const char* nul = NULL;
+    // const char* err_ = NULL;
 
     ticks t0 = getticks();
 
     stackstart = (char*)&argc;
-    STACKMAX = getstacksize() - 8192;
+    STACKMAX = sys_stackSize() - 8192;
 
-    start(NULL,
+    start(NULL
 #ifdef DEBUG
-        "nowhere", 0,
+        ,
+        "start(args)"
 #endif
-        err_);
+    );
 
     double dt = elapsed(getticks(), t0) / 1e9;
-    if (*err_ == ERROR_TRACE) {
+    if (err_ == ERROR_TRACE) {
         printf("[%.3fs] Aborted due to an unhandled error.\n", dt);
 #ifndef DEBUG
         printf("(run in debug mode to see a backtrace)\n");
 #endif
-    } else if (*err_ == NULL) {
+    } else if (err_ == NULL) {
         printf("[%.3fs] Completed successfully.\n", dt);
     }
 
-    return *err_ ? 1 : 0;
+    return err_ ? 1 : 0;
 }
 
 #endif // CHECKSTD
