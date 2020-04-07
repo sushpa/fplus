933320e180baddefc077805090cfb07e3396d948
Sushant Pandurangi <sushpa@lav.mavt.ethz.ch>
2020-04-07 22:29:53 +0200

    Generate Lua code

--- checkcc/main.c
+++ checkcc/main.c
@@ -1716,6 +1716,8 @@ TypeTypes evalType()
 }
 void ASTExpr_gen(
     ASTExpr* this, int level, bool spacing, bool escapeStrings);
+void ASTExpr_genl(
+    ASTExpr* this, int level, bool spacing, bool escapeStrings);
 void ASTTypeSpec_gen(ASTTypeSpec* this, int level);
 
 void ASTVar_gen(ASTVar* this, int level)
@@ -1723,28 +1725,39 @@ void ASTVar_gen(ASTVar* this, int level)
     printf("%.*s%s%s", level, spaces,
         this->flags.isVar ? "var " : this->flags.isLet ? "let " : "",
         this->name);
-    if (this->typeSpec) {
-        if (!(this->init and this->init->kind == TKFunctionCall
-                and !strcmp(this->init->name, this->typeSpec->name))) {
-            printf(" as ");
-            ASTTypeSpec_gen(this->typeSpec, level + STEP);
-        }
-    } else {
-        // should make this Expr_defaultType and it does it recursively for
-        // [, etc
-        const char* ctyp = TokenKind_defaultType(
-            this->init ? this->init->kind : TKUnknown);
-        if (this->init and this->init->kind == TKArrayOpen)
-            ctyp = TokenKind_defaultType(
-                this->init->right ? this->init->right->kind : TKUnknown);
-        if (this->init and this->init->kind == TKFunctionCall
-            and *this->init->name >= 'A' and *this->init->name <= 'Z')
-            ctyp = NULL;
-        if (ctyp) printf(" as %s", ctyp);
+    // if (this->typeSpec) {
+    if (!(this->init and this->init->kind == TKFunctionCall
+            and !strcmp(this->init->name, this->typeSpec->name))) {
+        printf(" as ");
+        ASTTypeSpec_gen(this->typeSpec, level + STEP);
     }
+    // }
+    // else {
+    //     // should make this Expr_defaultType and it does it recursively
+    //     for
+    //     // [, etc
+    //     const char* ctyp = TokenKind_defaultType(
+    //         this->init ? this->init->kind : TKUnknown);
+    //     if (this->init and this->init->kind == TKArrayOpen)
+    //         ctyp = TokenKind_defaultType(
+    //             this->init->right ? this->init->right->kind : TKUnknown);
+    //     if (this->init and this->init->kind == TKFunctionCall
+    //         and *this->init->name >= 'A' and *this->init->name <= 'Z')
+    //         ctyp = NULL;
+    //     if (ctyp) printf(" as %s", ctyp);
+    // }
     if (this->init) {
         printf(" = ");
         ASTExpr_gen(this->init, 0, true, false);
+    }
+}
+void ASTVar_genl(ASTVar* this, int level)
+{
+    printf("%.*s%s%s", level, spaces,
+        this->flags.isVar or this->flags.isLet ? "local " : "", this->name);
+    if (this->init) {
+        printf(" = ");
+        ASTExpr_genl(this->init, 0, true, false);
     }
 }
 void ASTTypeSpec_genc(ASTTypeSpec* this, int level, bool isconst);
@@ -1818,7 +1831,13 @@ void ASTScope_gen(ASTScope* this, int level)
         puts("");
     }
 }
-
+void ASTScope_genl(ASTScope* this, int level)
+{
+    foreach (ASTExpr*, stmt, stmts, this->stmts) {
+        ASTExpr_genl(stmt, level, true, false);
+        puts("");
+    }
+}
 void ASTExpr_genc(ASTExpr* this, int level, bool spacing, bool inFuncArgs,
     bool escapeStrings);
 
@@ -1929,6 +1948,25 @@ void ASTType_gen(ASTType* this, int level)
         puts("");
     }
     puts("end type\n");
+}
+
+void ASTType_genl(ASTType* this, int level)
+{
+    if (not this->body) return; // printf("declare ");
+    printf("--[[\ntype %s", this->name);
+    if (this->super) {
+        printf(" extends ");
+        ASTTypeSpec_gen(this->super, level);
+    }
+    puts("");
+    // if (not this->body) return;
+
+    foreach (ASTExpr*, stmt, stmts, this->body->stmts) {
+        if (!stmt) continue;
+        ASTExpr_gen(stmt, level + STEP, true, false);
+        puts("");
+    }
+    puts("end type\n]]--\n");
 }
 
 void ASTTypeSpec_genc(ASTTypeSpec* this, int level, bool isconst)
@@ -2073,6 +2111,29 @@ void ASTFunc_gen(ASTFunc* this, int level)
     puts("end function\n");
 }
 
+void ASTFunc_genl(ASTFunc* this, int level)
+{
+    if (this->flags.isDeclare) return; // printf("declare ");
+
+    printf("function %s(", this->name);
+
+    foreach (ASTVar*, arg, args, this->args) {
+        ASTVar_genl(arg, level);
+        printf(args->next ? ", " : "");
+    }
+    printf(")\n");
+
+    // if (this->returnType) {
+    //     printf(" returns ");
+    //     ASTTypeSpec_gen(this->returnType, level);
+    // }
+    // puts("");
+    // if (this->flags.isDeclare) return;
+
+    ASTScope_genl(this->body, level + STEP);
+
+    puts("end\n");
+}
 const char* getDefaultValueForType(ASTTypeSpec* type)
 {
     if (!type) return ""; // for no type e.g. void
@@ -2369,6 +2430,157 @@ void ASTExpr_gen(ASTExpr* this, int level, bool spacing, bool escapeStrings)
 
         if (this->kind == TKPower and not spacing) putc(')', stdout);
         if (this->kind == TKArrayOpen) putc(']', stdout);
+    }
+}
+
+void ASTExpr_genl(
+    ASTExpr* this, int level, bool spacing, bool escapeStrings)
+{
+    // generally an expr is not split over several lines (but maybe in
+    // rare cases). so level is not passed on to recursive calls.
+    printf("%.*s", level, spaces);
+
+    switch (this->kind) {
+    case TKNumber:
+    case TKMultiDotNumber:
+    case TKRegex:
+    case TKInline:
+        printf("%.*s", this->strLength, this->value.string);
+        break;
+    case TKIdentifier:
+    case TKIdentifierResolved: {
+        char* tmp = (this->kind == TKIdentifierResolved) ? this->var->name
+                                                         : this->name;
+        printf("%s", tmp);
+    } break;
+
+    case TKString:
+        printf(escapeStrings ? "\\%.*s\\\"" : "%.*s\"", this->strLength - 1,
+            this->value.string);
+        break;
+
+    case TKLineComment:
+        printf("-- %.*s", this->strLength, this->value.string);
+        break;
+
+    case TKFunctionCall:
+    case TKFunctionCallResolved: {
+        char* tmp = (this->kind == TKFunctionCallResolved)
+            ? this->func->name
+            : this->name;
+        printf("%s(", tmp);
+        if (this->left) ASTExpr_genl(this->left, 0, false, escapeStrings);
+        printf(")");
+    } break;
+
+    case TKSubscript:
+    case TKSubscriptResolved: {
+        char* tmp = (this->kind == TKSubscriptResolved) ? this->var->name
+                                                        : this->name;
+        printf("%s[", tmp);
+        if (this->left) ASTExpr_genl(this->left, 0, false, escapeStrings);
+        printf("]");
+    } break;
+
+    case TKVarAssign:
+        // var x as XYZ = abc... -> becomes an ASTVar and an ASTExpr
+        // (to keep location). Send it to ASTVar_gen.
+        assert(this->var != NULL);
+        ASTVar_genl(this->var, 0);
+        break;
+
+    case TKKeyword_for:
+    case TKKeyword_if:
+    case TKKeyword_while:
+        printf("%s ", TokenKind_repr(this->kind, false));
+        if (this->left) ASTExpr_genl(this->left, 0, true, escapeStrings);
+        puts("");
+        if (this->body) ASTScope_genl(this->body, level + STEP);
+        printf("%.*send", level, spaces);
+        break;
+
+    default:
+        if (not this->opPrecedence) break;
+        // not an operator, but this should be error if you reach here
+        bool leftBr = this->left and this->left->opPrecedence
+            and this->left->opPrecedence < this->opPrecedence;
+        bool rightBr = this->right and this->right->opPrecedence
+            and this->right->kind
+                != TKKeyword_return // found in 'or return'
+            and this->right->opPrecedence < this->opPrecedence;
+
+        // if (this->kind == TKOpColon) {
+        //     // expressions like arr[a:x-3:2] should become
+        //     // arr[a:(x-3):2]
+        //     // or list literals [8, 9, 6, 77, sin(c)]
+        //     if (this->left) switch (this->left->kind) {
+        //         case TKNumber:
+        //         case TKIdentifier:
+        //         case TKString:
+        //         case TKOpColon:
+        //         case TKMultiDotNumber:
+        //         case TKUnaryMinus:
+        //             break;
+        //         default:
+        //             leftBr = true;
+        //         }
+        //     if (this->right) switch (this->right->kind) {
+        //         case TKNumber:
+        //         case TKIdentifier:
+        //         case TKString:
+        //         case TKOpColon:
+        //         case TKMultiDotNumber:
+        //         case TKUnaryMinus:
+        //             break;
+        //         default:
+        //             rightBr = true;
+        //         }
+        // }
+
+        //        if (false and this->kind == TKKeyword_return and
+        //        this->right) {
+        //            switch (this->right->kind) {
+        //            case TKString:
+        //            case TKNumber:
+        //            case TKIdentifier:
+        //            case TKFunctionCall:
+        //            case TKSubscript:
+        //            case TKRegex:
+        //            case TKMultiDotNumber:
+        //                break;
+        //            default:
+        //                rightBr = true;
+        //                break;
+        //            }
+        //        }
+
+        // if (this->kind == TKPower and not spacing) putc('(', stdout);
+
+        char lpo = leftBr and this->left->kind == TKOpColon ? '{' : '(';
+        char lpc = leftBr and this->left->kind == TKOpColon ? '}' : ')';
+        if (leftBr) putc(lpo, stdout);
+        if (this->left)
+            ASTExpr_genl(this->left, 0,
+                spacing and !leftBr and this->kind != TKOpColon,
+                escapeStrings);
+        if (leftBr) putc(lpc, stdout);
+
+        printf("%s",
+            this->kind == TKArrayOpen
+                ? "{"
+                : TokenKind_repr(this->kind, spacing));
+
+        char rpo = rightBr and this->right->kind == TKOpColon ? '{' : '(';
+        char rpc = rightBr and this->right->kind == TKOpColon ? '}' : ')';
+        if (rightBr) putc(rpo, stdout);
+        if (this->right)
+            ASTExpr_genl(this->right, 0,
+                spacing and !rightBr and this->kind != TKOpColon,
+                escapeStrings);
+        if (rightBr) putc(rpc, stdout);
+
+        // if (this->kind == TKPower and not spacing) putc(')', stdout);
+        if (this->kind == TKArrayOpen) putc('}', stdout);
     }
 }
 
@@ -2674,6 +2886,23 @@ void ASTModule_gen(ASTModule* this, int level)
     foreach (ASTFunc*, func, funcs, this->funcs)
         ASTFunc_gen(func, level);
 }
+void ASTModule_genl(ASTModule* this, int level)
+{
+    printf("-- module %s\n", this->name);
+
+    // foreach (ASTImport*, import, imports, this->imports)
+    //     ASTImport_gen(import, level);
+
+    // puts("");
+
+    foreach (ASTType*, type, types, this->types)
+        ASTType_genl(type, level);
+
+    foreach (ASTFunc*, func, funcs, this->funcs)
+        ASTFunc_genl(func, level);
+
+    puts("start()");
+}
 
 void ASTModule_genc(ASTModule* this, int level)
 {
@@ -2702,6 +2931,7 @@ void ASTModule_genc(ASTModule* this, int level)
 typedef enum ParserMode {
     PMLint,
     PMGenC,
+    PMGenLua,
     PMGenCpp,
     PMGenJavaScript,
     PMGenWebAsm,
@@ -2809,7 +3039,7 @@ Parser* Parser_fromFile(char* filename, bool skipws)
         ret->token.kind = TKUnknown;
         ret->token.line = 1;
         ret->token.col = 1;
-        ret->mode = PMGenC; // parse args to set this
+        ret->mode = PMGenLua; // parse args to set this
         ret->errCount = 0;
         ret->warnCount = 0;
         ret->errLimit = 20;
@@ -4197,6 +4427,10 @@ int main(int argc, char* argv[])
                 ASTModule_gen(mod, 0);
         } break;
 
+        case PMGenLua: {
+            foreach (ASTModule*, mod, mods, modules)
+                ASTModule_genl(mod, 0);
+        } break;
         case PMGenC: {
             printf("#include \"checkstd.h\"\n");
             Parser_genc_open(parser);
